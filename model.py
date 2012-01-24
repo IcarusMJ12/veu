df67ebc517328c290548960eeda9d3905418e108
megawidget <megawidget@gmail.com>
2012-01-23 23:25:39 -0800

    added a license -- Creative Commons BY-NC-SA

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+#Copyright (c) 2012 Igor Kaplounenko
+#This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
 import re
 from datetime import datetime
 from copy import deepcopy
--- nom.py
+++ nom.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#Copyright (c) 2012 Igor Kaplounenko
+#This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 
 import ply.lex as lex
 import ply.yacc as yacc
