1dcf6db478421554ff7fb2e284d6c0bebca0e69f
megawidget <megawidget@gmail.com>
2012-02-24 18:09:53 -0800

    added caching

--- veu.py
+++ veu.py
@@ -14,7 +14,9 @@ import logging
 import types
 import re
 import glob
-from os.path import basename, splitext
+from os.path import basename, splitext, exists
+from os import makedirs
+import shutil
 import getpass
 
 from nom import nom
@@ -27,30 +29,50 @@ RE_LOG_POSITION=re.compile(r'Bounding Box of ([0-9]+) => \(([0-9]+),([0-9]+)\) -
 DEFAULT_FONTS='andalemono,bitstreamverasansmono,lucidaconsole'
 DEFAULT_PATH='/Users/'+getpass.getuser()+'/Library/Application Support/Steam/SteamApps/common/europa universalis iii - complete/Data/'
 TEXT_COLOR=(255,255,255)
+CACHE_DIR='./cache/'
 
 def units(population, base_tax):
     return min(0.99+float(population)/100000, 2)+float(base_tax)/20
 
 class Map(object):
-    def __init__(self, image, logger):
-        self._logger=logger
+    def __init__(self, image=None, cache=None, scale=MAP_SCALE, loglevel=logging.INFO):
+        self._logger=logging.getLogger(name=self.__class__.__name__, loglevel=loglevel)
         self._provinces=image
         self._edges=None
         self._countries=None
-        self._scale=1
+        self._scale=scale
+        self._cache=cache
+        provinces_cache_path=self._cache+'provinces'+str(self._scale)+'.png'
+        edges_cache_path=self._cache+'edges'+str(self._scale)+'.png'
+        if self._cache!=None:
+            if not exists(self._cache):
+                makedirs(self._cache)
+            try:
+                self._provinces=Image.open(provinces_cache_path)
+                self._edges=Image.open(edges_cache_path)
+            except IOError:
+                self._logger.info("cache appears to be empty")
+                self._upscale()
+                self._findEdges()
+                self._provinces.save(provinces_cache_path)
+                self._edges.save(edges_cache_path)
+        else:
+            assert(image!=None)
+            self._upscale()
+            self._findEdges()
     
-    def upscale(self, amount):
+    def _upscale(self):
         self._logger.info("upscaling...")
-        assert(isinstance(amount, int))
-        if amount>1:
-            new_size=(self._provinces.size[0]*amount, self._provinces.size[1]*amount)
+        assert(isinstance(self._scale, int))
+        if self._scale>1:
+            new_size=(self._provinces.size[0]*self._scale, self._provinces.size[1]*self._scale)
             self._logger.info('\tresizing...')
             self._provinces=self._provinces.resize(new_size,Image.NEAREST)
             if 'weave' not in globals():
                 self._logger.warn('\tscipy.weave was not imported; expect ugly-looking maps due to lack of smoothing')
             else:
                 self._logger.info('\tsmoothing...')
-                r=amount/2
+                r=self._scale/2
                 s=array(self._provinces)
                 x, y = s.shape[:2]
                 d=s.copy()
@@ -58,9 +80,8 @@ class Map(object):
                     smoothing_code=f.read()
                 weave.inline(smoothing_code, ['s','d','x','y','r'], type_converters=weave.converters.blitz)
                 self._provinces=Image.fromarray(d)
-            self._scale*=amount
     
-    def findEdges(self):
+    def _findEdges(self):
         self._logger.info("finding edges...")
         image=self._provinces.filter(ImageFilter.FIND_EDGES)
         image=image.convert('L')
@@ -69,11 +90,6 @@ class Map(object):
         image=ImageOps.colorize(image, (255,255,255), (0,0,0)).convert('RGBA')
         image.putalpha(alpha)
         self._edges=image
-
-    def getEdges(self):
-        if not self._edges:
-            self.findEdges()
-        return self._edges
 
     def getShadedMap(self, color_map=None, default=None):
         result=self._provinces
@@ -88,11 +104,7 @@ class Map(object):
                     except KeyError:
                         if default:
                             data[x,y]=default
-        edges=self.getEdges()
-        return Image.composite(result, edges, edges)
-
-    def getProvinces(self):
-        return self._provinces
+        return Image.composite(result, self._edges, self._edges)
 
 def _loadPositionsFromLog(self):
     self._logger.debug("parsing out positions...")
@@ -194,7 +206,7 @@ class Veu(object):
                 except KeyError as e:
                     self._logger.error(filename+': '+str(e)+' '+str(d))
 
-    def showMap(self, destination=None, date=the_beginning, savename=None):
+    def showMap(self, destination=None, date=the_beginning, savename=None, cache=CACHE_DIR):
         pygame.display.init()
         pygame.display.set_caption('veu running...')
         pygame.display.set_mode((200,1))
@@ -221,8 +233,7 @@ class Veu(object):
                     color_map[self.province_colors[province['code']]]=(0,0,255,255)
 
         #render map
-        m=Map(Image.open(self.map_path).convert('RGBA'), self._logger)
-        m.upscale(MAP_SCALE)
+        m=Map(Image.open(self.map_path).convert('RGBA'), cache='cache/', scale=MAP_SCALE, loglevel=self._logger.getEffectiveLevel())
         image=m.getShadedMap(color_map)
         map_size=image.size
         screen=pygame.Surface(map_size)
@@ -278,12 +289,17 @@ if __name__ == '__main__':
     parser.add_argument('--savefile','-s',nargs=1,help="load data from save file instead")
     parser.add_argument('--font','-f',nargs=1,help="use this system font instead")
     parser.add_argument('--scale','-S',nargs=1,help="scale up the map (defaults to "+str(MAP_SCALE)+")")
+    parser.add_argument('--cache','-c',nargs=1,help="use specified directory for cache rather than the default '"+CACHE_DIR+"'")
+    parser.add_argument('--clear-cache','-C',action='store_true',help="Clear the image cache before processing map.  Useful if scipy.weave has been added, or if the cache has been bloated by images with unnecessary image sizes.")
     options=parser.parse_args()
     loglevel=logging.DEBUG if options.verbose else logging.INFO
     logging.basicConfig(level=loglevel)
     logger=logging.getLogger('veu')
     path = options.path[0] if options.path else DEFAULT_PATH
     veu=Veu(path, logger)
+    cache=options.cache[0] if options.cache else CACHE_DIR
+    if options.clear_cache:
+        shutil.rmtree(cache)
     if options.scale:
         MAP_SCALE=int(options.scale[0])
     if options.map:
@@ -296,6 +312,6 @@ if __name__ == '__main__':
             logger.critical("Output image must be specified!")
             exit(1)
         else:
-            veu.showMap(options.output[0] if options.output else None, options.date[0] if options.date else the_beginning, options.savefile[0] if options.savefile else None)
+            veu.showMap(options.output[0] if options.output else None, options.date[0] if options.date else the_beginning, options.savefile[0] if options.savefile else None, cache)
     elif options.action[0]=='stats':
         logger.info("Not implemented.")
